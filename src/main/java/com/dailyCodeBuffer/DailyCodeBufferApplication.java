8289718e84fee3b0bd869c91abae5aeae927ec09
xdeepakkumar <xdeepakkumar355@gmail.com>
2021-10-10 18:05:20 +0530

    application created with H2 database config done

---
+++ src/main/java/com/dailyCodeBuffer/DailyCodeBufferApplication.java
@@ -0,0 +1,13 @@
+package com.dailyCodeBuffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyCodeBufferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyCodeBufferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dailyCodeBuffer/DailyCodeBufferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyCodeBuffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyCodeBufferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
